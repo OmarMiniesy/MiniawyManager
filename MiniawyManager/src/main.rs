9b39f71cb15a83b1d7edb06086becef1decbbf12
OmarMiniesy <omarminiesy2003@gmail.com>
2023-03-18 14:22:24 +0200

    Viewing Process information

--- MiniawyManager/src/main.rs
+++ MiniawyManager/src/main.rs
@@ -1,6 +1,26 @@
+use procfs::process::Process;
+use procfs::process::all_processes;
+
 fn main() {
-    println!("Hello, world!");
-    println!("I'm a Rustacean!");
-    println!("zizooo");
-    sognasrvasrj;varjv 
+
+    for process in all_processes().unwrap() {
+        let process = process.unwrap();
+        let stat = process.stat().unwrap();
+        let status = process.status().unwrap();
+        println!("{:?}", status);
+        // println!("Name: {}", stat.comm);
+        // println!("PID: {}", stat.pid);
+        // println!("Parent PID: {}", stat.ppid);
+        // println!("State: {}", stat.state);
+        // println!("Priority: {}", stat.priority);
+        // println!("Nice value: {}", stat.nice);
+        // println!("Start time: {}", stat.starttime);
+        // println!("Virtual memory size: {}", stat.vsize);
+        // println!("Resident set size: {}", stat.rss);
+        // println!("Number of threads: {}", stat.num_threads);
+        // println!("CPU time: {}s", stat.utime + stat.stime);
+        // //user of a process
+        // // println!("User: {}", stat.uid);
+        // println!("");
+    }
 }
