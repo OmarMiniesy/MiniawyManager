0f9750a91650c3a45f30138938abff53def2919f
OmarMiniesy <omarminiesy2003@gmail.com>
2023-04-11 00:45:36 +0200

    fixed the cpu usage, tried using sysinfo

--- MiniawyManager/src/main.rs
+++ MiniawyManager/src/main.rs
@@ -1,48 +1,42 @@
 use procfs::process::Process;
 use procfs::process::all_processes;
-use psutil::process::Process as PsutilProcess;
 use users::{get_user_by_uid, User, get_group_by_gid, Group};
 use std::fs;
-
-//use std::time::{Duration, SystemTime};
+use sysinfo::{System, SystemExt, ProcessExt, NetworkExt};
 
 fn main() {
 
-    //let mut totalcpu :f64 = 0.0;
+    // let mut system = System::new_all();
+    // system.refresh_all();
 
     for process in all_processes().unwrap() {
         let process = process.unwrap();
         let stat = process.stat().unwrap();
         let status = process.status().unwrap();
+
         let cputime = stat.utime + stat.stime;
 
-        // let start_time = Duration::from_nanos(stat.starttime as u64 * 1_000_000);
-        // let elapsed_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)
-        //     .unwrap_or_else(|_| Duration::new(0, 0));
-        // let total_time = stat.utime + stat.stime + stat.cutime as u64 + stat.cstime as u64;
-        // let cpu_percent = 100.0 * total_time as f64 / elapsed_time.as_secs_f64();
-        // totalcpu = totalcpu + cpu_percent;
-
-        // let elapsed_time = SystemTime::now().duration_since(start_time);
-        // let total_time = stat.utime + stat.stime;
-        // let cpu_percent = 100.0 * total_time as f64 / elapsed_time.as_secs_f64();
+        let starttime = stat.starttime;
+        let cpu_usage = 100.0 * cputime as f64/starttime as f64;
 
         // println!("{:?}", status);
         // println!("");
         // println!("{:?}", stat);
         // println!("");
+
         println!("Name: {}", stat.comm);
         println!("PID: {}", stat.pid);
         println!("Parent PID: {}", stat.ppid);
         println!("State: {}", stat.state);
         println!("Priority: {}", stat.priority);
         println!("Nice value: {}", stat.nice);
-        println!("Start time: {}", stat.starttime);
         println!("Virtual memory size: {}", stat.vsize);
         println!("Resident set size: {}", stat.rss);
         println!("Number of threads: {}", stat.num_threads);
         println!("CPU time: {}", cputime);
+        println!("CPU usage: {:.4}%", cpu_usage);
         println!("User ID: {}", status.ruid);
+        
         match get_user_by_uid(status.ruid) {
             Some(user) => println!("User name: {}", user.name().to_string_lossy()),
             None => println!("User not found for ruid {}", status.ruid),
@@ -52,12 +46,43 @@ fn main() {
             Some(group) => println!("Group name: {}", group.name().to_string_lossy()),
             None => println!("Group not found for rgid {}", status.rgid),
         }
-        
-
-        //println!("Elapsed time: {:?}", elapsed_time);
-        //println!("CPU Percentage: {}%" , cpu_percent);
         println!("");
     }
 
-    //println!("Total CPU Usage: {}%", totalcpu);
 }
+
+
+// use sysinfo::{Pid, NetworkExt, NetworksExt,ProcessExt, System, SystemExt};
+
+// fn main() {
+//     // Create a new system object
+//     let mut system = System::new_all();
+
+//     // Refresh all system information
+//     system.refresh_all();
+
+//     for (pid, process) in system.processes() {
+
+//         println!("Process name: {}", process.name());
+//         println!("Process ID: {}", pid);
+        
+//         println!("Process CPU usage: {:.4}%", process.cpu_usage());
+//         println!("Process memory usage: {} KB", process.memory());
+    
+//         let status = process.status();
+//         match(status) {
+//             sysinfo::ProcessStatus::Run => println!("Process is running"),
+//             sysinfo::ProcessStatus::Sleep => println!("Process is sleeping"),
+//             sysinfo::ProcessStatus::Stop => println!("Process is stopped"),
+//             sysinfo::ProcessStatus::Zombie => println!("Process is zombie"),
+//             sysinfo::ProcessStatus::Idle => println!("Process is idle"),
+//             other => println!("Process is in unknown state: {:?}", other),
+//         }
+
+//         println!("\n");
+//     }
+
+
+// }
+
+
